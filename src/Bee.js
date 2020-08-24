c7412a497963f50f1b6961d43a7657db816c78cf
Katie Poa <kpoa@berkeley.edu>
2020-08-24 11:07:34 -0700

    Implement Bee subclass (built off of Grub) passes tests

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,10 @@
 var Bee = function() {
+  var bee = new Grub();
+  this.age = 5;
+  this.color = 'yellow';
+  this.food = bee.food;
+  this.job = 'keep on growing';
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+var Bee.prototype.constructor = Bee;
