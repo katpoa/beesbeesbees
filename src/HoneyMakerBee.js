2446012d6db14803be207957f19773d320703791
Katie Poa <kpoa@berkeley.edu>
2020-08-24 11:40:37 -0700

    Implement HoneyMakerBee subclass of Bee - passes tests!

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
-var HoneyMakerBee = function() {
+var HoneyMakerBee = function(color, food) {
+  Bee.call(this, color);
+  Bee.call(this, food);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
