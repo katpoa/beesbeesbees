449d3ab859b0d6a9df8b50d0b94d58f4f9332645
Katie Poa <kpoa@berkeley.edu>
2020-08-24 11:44:42 -0700

    Implement ForagerBee subclass from Bee - passes all tests!

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,17 @@
-var ForagerBee = function() {
+var ForagerBee = function(color, food) {
+  Bee.call(this, color);
+  Bee.call(this, food);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
+};
+
+ForagerBee.prototype = Object.create(Bee.prototype);
+
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
 };
 
